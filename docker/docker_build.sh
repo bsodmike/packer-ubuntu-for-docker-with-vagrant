88287e915d7fcc1401398748afa5a1876fad4522
Michael de Silva <michael@mwdesilva.com>
2014-10-24 03:29:16 +0530

    WIP Use mysql 5.7 image from docker hub

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 docker build -t inertialbox/trusty-base ~/hack_ops/packer-digitalocean-docker/docker/trusty_base
-docker build -t inertialbox/mysql ~/hack_ops/packer-digitalocean-docker/docker/mysql/5.6
--- docker/docker_run_alpha.sh
+++ docker/docker_run_alpha.sh
@@ -2,7 +2,14 @@
 
 APP_NAME=app
 
-docker run --name mysql-data -v /var/lib/mysql inertialbox/trusty-base /bin/true
-#docker run -d --volumes-from mysql-data -v /etc/mysql:/etc/mysql --name mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=0mDF30W43I" -e "MYSQL_USER=appuser" -e "MYSQL_PASSWORD=A307W7oP52j6Fxv" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
-docker run -d --volumes-from mysql-data -v /etc/mysql:/etc/mysql --name mysql -p 3306:3306 -e "MYSQL_PASS=0mDF30W43I" inertialbox/mysql
-docker run -ti --rm --name web --link mysql:webdb inertialbox/trusty-base /bin/bash
+docker ps -a | grep mysql-data > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  docker run --name mysql-data -v /var/lib/mysql inertialbox/trusty-base /bin/true
+fi
+
+docker ps -a | grep "mysql[^\-]" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  docker run -d --volumes-from mysql-data -v /etc/mysql:/etc/mysql --name mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=0mDF30W43I" -e "MYSQL_USER=appuser" -e "MYSQL_PASSWORD=A307W7oP52j6Fxv" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
+fi
+
+docker run -ti --rm --volumes-from mysql-data --name web --link mysql:webdb inertialbox/trusty-base /bin/bash
