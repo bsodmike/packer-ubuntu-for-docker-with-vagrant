ef75f6ed5f6a0c8459de096f0c446e9acd0955c1
Michael de Silva <michael@mwdesilva.com>
2014-10-24 05:20:42 +0530

    :clap: First boot

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build -t inertialbox/trusty-base ~/hack_ops/packer-digitalocean-docker/docker/trusty_base
+docker build -t inertialbox/rails-nginx-unicorn ~/hack_ops/packer-digitalocean-docker/docker/rails-nginx-unicorn
