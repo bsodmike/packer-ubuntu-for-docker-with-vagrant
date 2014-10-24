1afc0e21f4ab926a636575374aa8b172b4748511
Michael de Silva <michael@mwdesilva.com>
2014-10-24 06:44:35 +0530

    Fix permissions to ensure assets are served & update script to allow rapid rebuilding

--- docker/docker_run_alpha.sh
+++ docker/docker_run_alpha.sh
@@ -12,5 +12,12 @@ if [ $? -ne 0 ]; then
   docker run -d -v /tmp:/tmp -v /etc/mysql:/etc/mysql -v /var/lib/mysql:/var/lib/mysql --name mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=0mDF30W43I" -e "MYSQL_USER=${APP_NAME}" -e "MYSQL_PASSWORD=A307W7oP52j6Fxv" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
 fi
 
-docker build -t inertialbox/inertialbox-app ~/hack/inertialbox.com
-docker run -d --name app -v /var/lib/mysql:/var/lib/mysql -v /tmp:/tmp -p 80:80 -e "SECRET_KEY_BASE=secretkey" --link mysql:webdb inertialbox/inertialbox-app
+[ -n "$REBUILD" ] && docker build -t inertialbox/inertialbox-app ~/hack/inertialbox.com
+
+docker ps -a | grep "[^-]app\b" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  docker stop app && docker rm app
+fi
+docker run -d --name app -v /var/log/nginx/:/var/log/nginx/ -v /var/lib/mysql:/var/lib/mysql -v /tmp:/tmp -p 80:80 -e "SECRET_KEY_BASE=secretkey" --link mysql:webdb inertialbox/inertialbox-app
+
+echo -e "\n===> Done.\n"
