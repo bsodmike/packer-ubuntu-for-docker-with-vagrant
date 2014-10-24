b9698cd1b65f24fef9506ce7439f2d5a9c2ab8eb
Michael de Silva <michael@mwdesilva.com>
2014-10-24 07:02:06 +0530

    Using host volumes instead of data containers

--- docker/docker_run_alpha.sh
+++ docker/docker_run_alpha.sh
@@ -2,11 +2,6 @@
 
 APP_NAME=inertialbox
 
-#docker ps -a | grep mysql-data > /dev/null 2>&1
-#if [ $? -ne 0 ]; then
-  #docker run --name mysql-data -v /var/lib/mysql inertialbox/trusty-base /bin/true
-#fi
-
 docker ps -a | grep "mysql[^\-]" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   docker run -d -v /tmp:/tmp -v /etc/mysql:/etc/mysql -v /var/lib/mysql:/var/lib/mysql --name mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=0mDF30W43I" -e "MYSQL_USER=${APP_NAME}" -e "MYSQL_PASSWORD=A307W7oP52j6Fxv" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
