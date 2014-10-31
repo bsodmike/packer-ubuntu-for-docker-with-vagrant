5f612e7ddd85a0c0dcf1485b321af45e3967f8cc
Michael de Silva <michael@mwdesilva.com>
2014-11-01 03:39:41 +0530

    Extract docker related files into a new repo

--- docker/docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t inertialbox/trusty-base ~/hack_ops/packer-digitalocean-docker/docker/trusty_base
-docker build -t inertialbox/rails-nginx-unicorn ~/hack_ops/packer-digitalocean-docker/docker/rails-nginx-unicorn
